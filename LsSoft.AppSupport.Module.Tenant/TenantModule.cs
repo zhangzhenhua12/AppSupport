c5d1e7ad4fde901c4b6426dc715497695685ac7d
华仔很忙 <457233665@qq.com>
2021-08-07 21:39:13 +0800

    添加项目文件。

---
+++ LsSoft.AppSupport.Module.Tenant/TenantModule.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Volo.Abp.Modularity;
+
+namespace LsSoft.AppSupport.Module.Tenant
+{
+  public class TenantModule: AbpModule
+  {
+    public override void ConfigureServices(ServiceConfigurationContext context)
+    {
+      
+    }
+  }
+}
