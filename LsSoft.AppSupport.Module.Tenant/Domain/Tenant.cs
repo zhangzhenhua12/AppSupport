8a6142d5c268288d0e467ede430dd617d90f9b8b
华仔很忙 <457233665@qq.com>
2021-08-10 09:57:14 +0800

    11

---
+++ LsSoft.AppSupport.Module.Tenant/Domain/Tenant.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Entities;
+
+namespace LsSoft.AppSupport.Module.Tenant.Domain
+{
+  /// <summary>
+  /// 租户聚合根
+  /// </summary>
+  public class Tenant: AggregateRoot<string>
+  {
+    /// <summary>
+    /// 编号
+    /// </summary>
+    public string Number { get; set; }
+
+    /// <summary>
+    /// 描述信息
+    /// </summary>
+    public string Description { get; set; }
+
+    public Tenant()
+    {
+
+    }
+    public void AddTenant()
+    {
+
+    }
+
+  }
+}
